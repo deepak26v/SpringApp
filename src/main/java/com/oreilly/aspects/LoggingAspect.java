da73751ed241f849038a0da4d61415b2a57da6ea
Rohit Sharma <rs@gmail.com>
2016-04-16 18:29:12 -0400

    First Commit

--- src/main/java/com/oreilly/aspects/LoggingAspect.java
+++ src/main/java/com/oreilly/aspects/LoggingAspect.java
@@ -1,7 +1,21 @@
 package com.oreilly.aspects;
 
-/**
- * Created by deepakvalechha on 4/15/16.
- */
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import java.util.logging.Logger;
+
+@Aspect
+@Component
 public class LoggingAspect {
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    //This is the pointcut definition
+    @Before("execution(void com.oreilly..*.set*(*))")
+    public void callSetter(JoinPoint joinPoint) {
+        String signature = joinPoint.getSignature().getName();
+        logger.info("setter called by pointcut @ " + signature);
+    }
 }
